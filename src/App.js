b2bedde69687c33df23adb44c011ac6c755bcd73
karpss <ola.fam10@gmail.com>
2022-05-02 16:46:21 +0100

    Added loading state that was earlier missed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { createContext, useReducer } from "react";
+import React, {useState, useEffect, createContext, useReducer } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import { initialState, reducer } from "./redux/index";
+import Loadingscreen from "./components/Loadingscreen";
 
 
 export const GitHubAuthContext = createContext();
 
 function App() {
+  
+  const [loading, setLoading] = useState(true)
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
+
   return (
-    
+    <>
+    {loading === false ? (
     <GitHubAuthContext.Provider
       value={{
         state,
@@ -25,9 +33,18 @@ function App() {
         </Switch>
       </Router>
     </GitHubAuthContext.Provider>
+
+) : (
+  <Loadingscreen />
+)}
+
+
+    </>
    
     
   );
+  
+  
 }
 
 export default App;
---
+++ src/components/Loadingscreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loadingscreen = () => {
+  return (
+    <div>Loading....</div>
+  )
+}
+
+export default Loadingscreen;
